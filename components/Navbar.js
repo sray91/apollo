7b275e5bd9f604b6b0d5b60235160b01d3d8f1e5
swanagan <swanaganray@gmail.com>
2022-05-28 19:51:46 -0400

    saving before dinner

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,8 +1,15 @@
 import React from 'react'
+import Image from 'next/image'
 
 const Navbar = () => {
   return (
     <div className="text-white h-20">
+        <Image
+            src="/apolloLogo.svg"
+            alt="Apollo Systems logo"
+            width={50}
+            height={50}
+        />
         Apollo Systems
     </div>
   )
