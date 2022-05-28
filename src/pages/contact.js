faa86020c21bd8db04bd77394fdeb65297171ddc
swanagan <swanaganray@gmail.com>
2022-05-27 22:00:27 -0400

    added stuff

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,5 +1,5 @@
 import Head from 'next/head'
-import Navbar from '../src/components/Navbar'
+import Navbar from '../components/Navbar'
 import styles from '../styles/Home.module.css'
 
 export default function Contact() {
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,14 @@
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: [
+            "./src/pages/*.{html,js}", 
+            "./src/components/*.{html,js}", 
+          ],
   theme: {
-    extend: {},
-  },
-  plugins: [],
+    extend: {
+      fontFamily: {
+        'poppins': ['Poppins', 'sans-serif'],
+      },
+    },
+    plugins: [],
+  }
 }
