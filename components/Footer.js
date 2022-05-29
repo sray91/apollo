2569a059b458f5c36f44de222a631816878d02e1
swanagan <swanaganray@gmail.com>
2022-05-29 11:26:49 -0400

    font awesome config done

--- components/Footer.js
+++ components/Footer.js
@@ -1,25 +1,34 @@
 import React from 'react'
 import Link from 'next/link'
-// Import the FontAwesomeIcon component
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-
-// import the icons you need
-import {
-  faSearch,
-  faAmbulance,
-  faAnchor,
-} from "@fortawesome/free-solid-svg-icons";
+import { faTwitter, 
+         faLinkedin, 
+         faFacebook, 
+         faInstagram } from '@fortawesome/free-brands-svg-icons'
 
 const Footer = () => {
   return (
     <div className="bg-black text-white flex justify-center align-middle h-20">
       <Link href="https://linkedin.com">
-        <a className="px-6">LinkedIn</a>
+        <a className="p-6">
+          <FontAwesomeIcon icon={faLinkedin} size="2x"/>
+        </a>
       </Link>
       <Link href="https://twitter.com">
-        <a className="px-6">Twitter</a>
+        <a className="p-6">
+          <FontAwesomeIcon icon={faTwitter} size="2x"/>
+        </a>
       </Link>
-      <FontAwesomeIcon icon="fa-brands fa-twitter" />
+      <Link href="https://facebook.com">
+        <a className="p-6">
+          <FontAwesomeIcon icon={faFacebook} size="2x"/>
+        </a>
+      </Link>
+      <Link href="https://instagram.com">
+        <a className="p-6">
+          <FontAwesomeIcon icon={faInstagram} size="2x"/>
+        </a>
+      </Link>
     </div>
   )
 }
