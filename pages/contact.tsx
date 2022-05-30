0bd899f561519dfa44988776800aa91820c83545
swanagan <swanaganray@gmail.com>
2022-05-29 22:47:42 -0400

    more formatting

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -3,16 +3,18 @@ import Head from 'next/head'
 
 const contact = () => {
   return (
-    <div>
+    <div className="h-screen">
       <Head>
         <title>Contact Page</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div className="flex h-screen justify-center items-center">
+      <div className="flex justify-center items-center py-20">
         <h1 className="font-black text-8xl">Contact</h1>
       </div>
-      <div className="flex justify-center items-center">
+      <div className="flex justify-center items-center py-10">
         <p className="px-6">843-608-8485</p>
+      </div>
+      <div className="flex justify-center items-center">
         <p className="px-6">apollosystemsbiz@gmail.com</p>
       </div>
     </div>
