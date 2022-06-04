4ce2becd8e031bac461f034e52057aec05a7bb9b
swanagan <swanaganray@gmail.com>
2022-06-04 16:30:58 -0400

    added link to services section

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -42,24 +42,24 @@ const Sidebar = () => {
         </button>
       ) : 
       (
-        <button className="text-2xl text-white fixed top-4 right-4 z-20" onClick={() => setIsOpen(!isOpen)}>
+        <button className="text-5xl text-white fixed top-4 right-4 z-20" onClick={() => setIsOpen(!isOpen)}>
           x
         </button>
       )
     }
-      <div className={`top-0 right-0 fixed bg-purple-400/60 w-full h-full p-10 flex justify-center ${isOpen ? 'translate-x-0':'translate-x-full'} ease-in-out duration-300`}>
-        <div className="text-3xl text-white p-6 text-center font-bold">
+      <div className={`top-0 right-0 fixed bg-purple-400/90 w-full h-full p-10 flex justify-center ${isOpen ? 'translate-x-0':'translate-x-full'} ease-in-out duration-300`}>
+        <div className="text-5xl text-white p-6 text-center font-bold">
           <Link href="/">
-            <a><h2>Home</h2></a>
+            <a><h2 className="h-[15vh]">Home</h2></a>
           </Link>
           <Link href="#services-section">
-          <a><h2>Services</h2></a>
+          <a><h2 className="h-[15vh]">Services</h2></a>
           </Link>
           <Link href="/about">
-          <a><h2>About</h2></a>
+          <a><h2 className="h-[15vh]">About</h2></a>
           </Link>
           <Link href="/contact">
-          <a><h2>Contact</h2></a>
+          <a><h2 className="h-[15vh]">Contact</h2></a>
           </Link>
         </div>
       </div>
