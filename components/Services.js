30f763984a7aa05cfec1414a171f4bfe16338d1b
swanagan <swanaganray@gmail.com>
2022-06-04 14:39:57 -0400

    added new services symbols

--- components/Services.js
+++ components/Services.js
@@ -1,9 +1,30 @@
 import React from 'react'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faComputer, 
+         faGlobe,
+         faPhone,
+         faRouter,
+         faFloppyDisk } from '@fortawesome/free-solid-svg-icons'
 
 const Services = () => {
   return (
-    <div className="flex h-screen bg-white justify-center items-center">
-        <h1 className="font-black text-8xl">Services</h1>
+    <div className="h-screen bg-white">
+        <h1 className="flex font-black text-8xl justify-center items-center py-8">Services</h1>
+        <div className="flex justify-center items-center py-6">
+          <FontAwesomeIcon icon={faComputer} size="2x"/>
+        </div>
+        <div className="flex justify-center items-center py-6">
+          <FontAwesomeIcon icon={faGlobe} size="2x"/>
+        </div>
+        <div className="flex justify-center items-center py-6">
+          <FontAwesomeIcon icon={faPhone} size="2x"/>
+        </div>
+        <div className="flex justify-center items-center py-6">
+          <FontAwesomeIcon icon={faRouter} size="2x"/>
+        </div>        
+        <div className="flex justify-center items-center py-6">
+          <FontAwesomeIcon icon={faFloppyDisk} size="2x"/>
+        </div>
     </div>
   )
 }
