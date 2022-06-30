4e89bd613695d83312a91ece1a34d91a73e3329f
cahank2010 <75578277+cahank2010@users.noreply.github.com>
2022-06-30 16:05:43 -0400

    added our headshots

---
+++ components/Space.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Image from 'next/image'
+import styles from './layout.module.css';
+
+const Space = () => {
+  return (
+    <div className="flex h-72 flex-col items-center justify-center py-2 bg-white">
+
+    </div>
+  )
+}
+
+export default Space
--- pages/about.tsx
+++ pages/about.tsx
@@ -10,14 +10,54 @@ const about = () => {
         <title>ABOUT US Page</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div className={styles.aboutHeader}>ABOUT US</div>
+      <h1 className={styles.aboutHeader}>ABOUT US</h1>
       <br></br>
-      <div className={styles.Body}>Our owners and consultants have over 20 years of combined experience in everything from engineering, 
-          automation, internal and external IT systems, software implementation, software configuration and even hardware setup/installation.
-          We have worked with and for numerous fortune 500 companies as well as small to medium businesses. We understand everyone's business 
-          and goals are different; we cater our approach individually.
-        </div> 
-        <br></br>
+      <h1 className={styles.subhead1}>Critical skills built upon real-world experience.</h1>
+            <h2 className={styles.Body}>Apollo Systems is a Charleston owned and grown business. Our goal is to focus on meeting the technology
+            needs of Small and Medium businesses within the Charleston area, to help make them more successful.</h2><br></br>
+            <h2 className={styles.Body}>Our team has over 20 years of combined experience, and come armed with a variety of skills including:</h2>
+            <p className={styles.Body5}>
+              <ol>
+                <li>Engineering</li>
+                <li>Project management</li>
+                <li>Automation</li>
+                <li>Cybersecurity</li>
+                <li>Software Development</li>
+                <li>Software Implementation</li>
+                <li>Software Configuration</li>
+                <li>Web development</li>
+                <li>Hardware Setup and Installation</li>
+                <li>Internal and external IT systems</li></ol></p>
+
+      <h3 className={styles.subhead1}>What makes Apollo different?</h3>
+
+      <h4 className={styles.Body}>We have worked with and for mutliple companies of various sizes, including both fortune 500 companies 
+          and small to medium businesses. In the past, we have found ourselves in many of the situations our clients face everyday, and 
+          come armed with the learned skills to quickly produce results. We understand businesses and goals are different depending on the 
+          company and industry, and we work to develop partnerships with our clients that not only ensure their information technology and 
+          security needs are met, but that they are enabled to succeed longterm.</h4>
+ 
+      <h5 className={styles.subhead1}>Meet our team:</h5>
+
+      
+      <div className="flex gap-24 columns-3 content-center text-center items-center justify-center">
+          <div className="column">
+            <img src="./SwayRay.jpg" alt="Ryan" width="200" height="200px"></img>
+            <a>Swanagan Ray</a>
+          </div>
+          <div className="column">
+            <img src="./ryGuyNew.jpg" alt="Ryan" width="200" height="200px"></img>
+            <a>Ryan Wagner</a>
+          </div>
+          <div className="column">
+            <img src="./hanks.jpg" alt="Ryan" width="200" height="200px"></img>
+            <a>Cory Hank</a>
+          </div>
+      </div>
+
+       <br></br>
+
+
       <Link href="/contact">
         <button className={styles.collapsible}>FIND OUT MORE</button>
       </Link> 
